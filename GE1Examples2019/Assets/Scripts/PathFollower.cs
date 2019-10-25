8c0a786ea0904669e093c64dde39f828cc2eb5c3
UniversalEnya <Emmaneesoneire@gmail.com>
2019-10-25 16:54:08 +0100

    Trying the thing?

---
+++ GE1Examples2019/Assets/Scripts/PathFollower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    public Path path; //this is to get the waypoints from the Path script
+    public Vector3 currentWaypoint;
+
+    public void Update()
+    {
+        
+    }
+
+}
